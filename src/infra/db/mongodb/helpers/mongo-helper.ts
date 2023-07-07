1795b163bd816059ea2fb0a7563537248deed878
Caio César Roma <caioroma7@gmail.com>
2023-07-07 01:37:02 -0300

    refactor: retirando importacao sem uso

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
 import { Collection, ConnectOptions, MongoClient } from 'mongodb'
-import { AccountModel } from '../../../../domain/models/account'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
